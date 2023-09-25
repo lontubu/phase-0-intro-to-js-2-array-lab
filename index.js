ecb9aa9c5a953909e47405dc010d9f20076e2a02
lontubu <nickson.lontubu@studentmoringa.com>
2023-09-25 22:09:46 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat (name){
+    cats.push ("Ralph")
+
+};
+function destructivelyPrependCat (name){
+    cats.unshift ("Bob")   
+
+}; 
+function destructivelyRemoveLastCat (name) {
+    cats.pop ()    
+
+}; 
+function destructivelyRemoveFirstCat (name) {
+    cats.shift ()  
+
+};   
+function appendCat (name) {
+    const newCats = [...cats, "Broom"];
+    return newCats
+
+};
+function prependCat (name) {
+    const newestCats = ["Arnold", ...cats];
+    return newestCats
+
+};
+function removeLastCat (name) {
+    const newerCats = cats.slice(0, -1);
+    return newerCats;
+    
+};
+function removeFirstCat (name){
+    const neewCats = cats.slice(1);
+    return neewCats;
+
+};   
+
+
